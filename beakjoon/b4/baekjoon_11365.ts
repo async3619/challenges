dbb94be8c09ad145542df96c664fb447d155034c
Sophia <beyondthesophia@gmail.com>
2022-09-27 22:30:17 +0900

    🤦 add baekjoon 11365

---
+++ beakjoon/b4/baekjoon_11365.ts
@@ -0,0 +1,29 @@
+function solution(input: string[]) {
+    console.log(
+        input
+            .slice(0, input.length - 1)
+            .map(l => l.split("").reverse().join(""))
+            .join("\n"),
+    );
+}
+
+(callback => {
+    if (typeof process !== "undefined" && "env" in process && "arguments" in process.env && process.env.arguments) {
+        solution(process.env.arguments.split("\n"));
+        return;
+    }
+
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const input: string[] = [];
+    rl.on("line", line => {
+        input.push(line);
+    }).on("close", function () {
+        callback(input);
+        process.exit();
+    });
+})(solution);
