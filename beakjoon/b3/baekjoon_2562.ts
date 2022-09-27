84669f6628995211170105c138df8de550e8c4c2
Sophia <beyondthesophia@gmail.com>
2022-09-27 23:51:40 +0900

    🤦 add baekjoon 2562

---
+++ beakjoon/b3/baekjoon_2562.ts
@@ -0,0 +1,28 @@
+function solution(input: string[]) {
+    const p = input.map(p => parseInt(p));
+    const max = Math.max(...p);
+
+    console.log(`${max}
+${p.indexOf(max) + 1}`);
+}
+
+(callback => {
+    if (typeof process !== "undefined" && "env" in process && "arguments" in process.env && process.env.arguments) {
+        solution(process.env.arguments.split("\n"));
+        return;
+    }
+
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const input: string[] = [];
+    rl.on("line", line => {
+        input.push(line);
+    }).on("close", function () {
+        callback(input);
+        process.exit();
+    });
+})(solution);
