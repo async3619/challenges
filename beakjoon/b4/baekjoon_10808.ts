b3d52fbaa05e3960f12fab43073ab56cd10c33b0
Sophia <beyondthesophia@gmail.com>
2022-09-27 22:24:28 +0900

    🤦 add baekjoon 10808

---
+++ beakjoon/b4/baekjoon_10808.ts
@@ -0,0 +1,35 @@
+function solution([input]: string[]) {
+    const p = input.split("");
+    const a = "a".charCodeAt(0);
+    const cnt = "z".charCodeAt(0) - a + 1;
+    console.log(
+        new Array(cnt)
+            .fill(null)
+            .map((i, n) => {
+                const ch = String.fromCharCode(a + n);
+                return p.filter(c => c === ch).length;
+            })
+            .join(" "),
+    );
+}
+
+(callback => {
+    if (typeof process !== "undefined" && "env" in process && "arguments" in process.env && process.env.arguments) {
+        solution(process.env.arguments.split("\n"));
+        return;
+    }
+
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const input: string[] = [];
+    rl.on("line", line => {
+        input.push(line);
+    }).on("close", function () {
+        callback(input);
+        process.exit();
+    });
+})(solution);
