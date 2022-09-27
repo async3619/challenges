aa9fc22a0d6fab26c693c840b84cafc0235dbc71
Sophia <beyondthesophia@gmail.com>
2022-09-28 00:11:52 +0900

    🤦 add baekjoon 10818

---
+++ beakjoon/b3/baekjoon_10818.ts
@@ -0,0 +1,25 @@
+function solution([, data]: string[]) {
+    const p = data.split(" ").map(p => parseInt(p));
+    console.log(Math.min(...p), Math.max(...p));
+}
+
+(callback => {
+    if (typeof process !== "undefined" && "env" in process && "arguments" in process.env && process.env.arguments) {
+        solution(process.env.arguments.split("\n"));
+        return;
+    }
+
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const input: string[] = [];
+    rl.on("line", line => {
+        input.push(line);
+    }).on("close", function () {
+        callback(input);
+        process.exit();
+    });
+})(solution);
