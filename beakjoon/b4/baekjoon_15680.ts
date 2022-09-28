dd61e89d6bb6bbafb99ba713506a7afd6c0dbb62
Sophia <beyondthesophia@gmail.com>
2022-09-28 09:46:49 +0900

    🤦 add baekjoon 15680

---
+++ beakjoon/b4/baekjoon_15680.ts
@@ -0,0 +1,28 @@
+function solution([input]: string[]) {
+    if (input === "0") {
+        console.log("YONSEI");
+    } else {
+        console.log("Leading the Way to the Future");
+    }
+}
+
+(callback => {
+    if (typeof process !== "undefined" && "env" in process && "arguments" in process.env && process.env.arguments) {
+        solution(process.env.arguments.split("\n"));
+        return;
+    }
+
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const input: string[] = [];
+    rl.on("line", line => {
+        input.push(line);
+    }).on("close", function () {
+        callback(input);
+        process.exit();
+    });
+})(solution);
