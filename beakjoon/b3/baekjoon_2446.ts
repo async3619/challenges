ed0a94d862f15c9beb0ccc7b61aad459d07165ba
Sophia <beyondthesophia@gmail.com>
2022-09-28 22:25:24 +0900

    🤦 add baekjoon 2446

---
+++ beakjoon/b3/baekjoon_2446.ts
@@ -0,0 +1,46 @@
+function solution([input]: string[]) {
+    const n = parseInt(input);
+    const a: string[] = [];
+    for (let i = n - 1; i >= 0; i--) {
+        const aa: string[] = [];
+        for (let j = 0; j < n - i - 1; ++j) {
+            aa.push(" ");
+        }
+
+        aa.push(...new Array(i * 2 + 1).fill("*"));
+        a.push(aa.join(""));
+    }
+
+    for (let i = 1; i < n; i++) {
+        const aa: string[] = [];
+        for (let j = 0; j < n - i - 1; ++j) {
+            aa.push(" ");
+        }
+
+        aa.push(...new Array(i * 2 + 1).fill("*"));
+        a.push(aa.join(""));
+    }
+
+    console.log(a.join("\n"));
+}
+
+(callback => {
+    if (typeof process !== "undefined" && "env" in process && "arguments" in process.env && process.env.arguments) {
+        solution(process.env.arguments.split("\n"));
+        return;
+    }
+
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const input: string[] = [];
+    rl.on("line", line => {
+        input.push(line);
+    }).on("close", function () {
+        callback(input);
+        process.exit();
+    });
+})(solution);
