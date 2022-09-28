8684a6f6b12c653769129874654db0d8ab4033a0
Sophia <beyondthesophia@gmail.com>
2022-09-28 13:39:10 +0900

    🤦 add baekjoon 24883

---
+++ beakjoon/b4/baekjoon_24883.ts
@@ -0,0 +1,24 @@
+function solution([input]: string[]) {
+    console.log(input[0].toLowerCase() === "n" ? "Naver D2" : "Naver Whale");
+}
+
+(callback => {
+    if (typeof process !== "undefined" && "env" in process && "arguments" in process.env && process.env.arguments) {
+        solution(process.env.arguments.split("\n"));
+        return;
+    }
+
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const input: string[] = [];
+    rl.on("line", line => {
+        input.push(line);
+    }).on("close", function () {
+        callback(input);
+        process.exit();
+    });
+})(solution);
