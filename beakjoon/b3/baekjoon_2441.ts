dedba5eb1bf030d21a50cf5dc006826b58fccde4
Sophia <beyondthesophia@gmail.com>
2022-09-28 22:08:16 +0900

    🤦 add baekjoon 2441

---
+++ beakjoon/b3/baekjoon_2441.ts
@@ -0,0 +1,32 @@
+function solution([input]: string[]) {
+    const n = parseInt(input);
+    for (let i = 0; i < n; i++) {
+        console.log(
+            new Array(n)
+                .fill("*")
+                .map((_, _i) => (_i < i ? " " : "*"))
+                .join(""),
+        );
+    }
+}
+
+(callback => {
+    if (typeof process !== "undefined" && "env" in process && "arguments" in process.env && process.env.arguments) {
+        solution(process.env.arguments.split("\n"));
+        return;
+    }
+
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const input: string[] = [];
+    rl.on("line", line => {
+        input.push(line);
+    }).on("close", function () {
+        callback(input);
+        process.exit();
+    });
+})(solution);
