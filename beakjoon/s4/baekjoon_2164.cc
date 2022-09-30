8d85e364003158a79616e42c69a95c5f040a1306
Sophia <beyondthesophia@gmail.com>
2022-09-30 10:03:06 +0900

    🤦 add baekjoon 2164

---
+++ beakjoon/s4/baekjoon_2164.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int n = 0;
+    cin >> n;
+
+    queue<int> q;
+    for (int i = 0; i < n; ++i) {
+        q.push(i + 1);
+    }
+
+    while (q.size() > 1) {
+        q.pop();
+        auto i = q.front();
+        q.pop();
+        q.push(i);
+    }
+
+    cout << q.front() << endl;
+}
