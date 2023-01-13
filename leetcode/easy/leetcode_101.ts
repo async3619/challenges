811338e23733d9dc642a33104af79dd76695010a
Sophia <me@sophia-dev.io>
2023-01-14 03:04:56 +0900

    🤦 add leetcode 101

---
+++ leetcode/easy/leetcode_101.ts
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+function fromArray(inputArray: number[]) {
+    if (inputArray.length === 0) {
+        return null;
+    }
+
+    const root = new TreeNode(inputArray.shift());
+    const queue = [root];
+    while (queue.length > 0 && inputArray.length > 0) {
+        const curNode = queue.shift();
+        const leftVal = inputArray.shift();
+        const rightVal = inputArray.shift();
+
+        if (leftVal) {
+            curNode.left = new TreeNode(leftVal);
+            queue.push(curNode.left);
+        }
+        if (rightVal) {
+            curNode.right = new TreeNode(rightVal);
+            queue.push(curNode.right);
+        }
+    }
+
+    return root;
+}
+
+function seek(left: TreeNode | null, right: TreeNode | null) {
+    if (!left && !right) {
+        return true;
+    }
+
+    if (!left || !right) {
+        return false;
+    }
+
+    return left.val === right.val && seek(left.right, right.left) && seek(left.left, right.right);
+}
+
+function exec(root: TreeNode | null): boolean {
+    if (!root) {
+        return true;
+    }
+
+    return seek(root.left, root.right);
+}
+
+function isSymmetric(root: Array<number | null> | null): boolean {
+    if (!root) {
+        return false;
+    }
+
+    return exec(fromArray(root));
+}
