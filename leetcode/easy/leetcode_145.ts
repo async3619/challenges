813948a40928ede77d99e97b224c18ea7bf21601
Sophia <me@sophia-dev.io>
2023-01-14 11:50:20 +0900

    🤦 add leetcode 145

---
+++ leetcode/easy/leetcode_145.ts
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+function fromArray(inputArray: number[]) {
+    if (inputArray.length === 0) {
+        return null;
+    }
+
+    const root = new TreeNode(inputArray.shift());
+    const queue = [root];
+    while (queue.length > 0 && inputArray.length > 0) {
+        const curNode = queue.shift();
+        const leftVal = inputArray.shift();
+        const rightVal = inputArray.shift();
+
+        if (leftVal) {
+            curNode.left = new TreeNode(leftVal);
+            queue.push(curNode.left);
+        }
+        if (rightVal) {
+            curNode.right = new TreeNode(rightVal);
+            queue.push(curNode.right);
+        }
+    }
+
+    return root;
+}
+
+function seek(node: TreeNode | null, arr: number[]) {
+    if (!node) {
+        return;
+    }
+
+    if (node.left) {
+        seek(node.left, arr);
+    }
+
+    if (node.right) {
+        seek(node.right, arr);
+    }
+
+    arr.push(node.val);
+}
+
+function exec(node: TreeNode | null): number[] {
+    const ret: number[] = [];
+    seek(node, ret);
+
+    return ret;
+}
+
+function postorderTraversal(root: number[] | null): number[] {
+    return exec(fromArray(root));
+}
